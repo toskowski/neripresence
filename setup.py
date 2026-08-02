bbf74576cd2bae117da89ad29612fc75581f0e03
tosiek <89697662+toskowski@users.noreply.github.com>
2026-08-02 22:59:08 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+setup(
+  name = 'neripresence',         
+  packages = ['neripresence'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'Small and simple nerimity presence wrapper',   
+  author = 'Tosiek',                   
+  author_email = 'orzechowymastermind@proton.me',      
+  url = 'https://github.com/toskowski/neripresence',   
+  download_url = 'https://github.com/toskowski/neripresence/BLEHHH',    
+  keywords = ['nerimity', 'presence' ],   
+  install_requires=[            
+          'websockets',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      
+
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',   
+
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
+    'Programming Language :: Python :: 3.14'
+  ],
+)
