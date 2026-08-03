63094785a7950822acdbafe10001e8e4728c714e
tosiek <89697662+toskowski@users.noreply.github.com>
2026-08-03 13:23:59 +0200

    added a desc for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from distutils.core import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
   name = 'neripresence',         
   packages = ['neripresence'],   
   version = '0.4.1',      
   license='MIT',        
-  description = 'Small and simple nerimity presence wrapper :3',   
+  description = 'Small and simple nerimity presence wrapper :3',
+  long_description=long_description,
+  long_description_content_type='text/markdown'
   author = 'Tosiek',     
   url = 'https://github.com/toskowski/neripresence',   
   download_url = 'https://github.com/toskowski/neripresence/archive/refs/tags/v_041.tar.gz',    
